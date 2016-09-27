c0bd22f27ecbe9daa294a49e295557f8f985f246
Jason Cao <jc6933@nyu.edu>
2016-09-27 11:22:40 -0400

    changes

--- Plot_chi_square.py
+++ Plot_chi_square.py
@@ -1,47 +1,3 @@
-import numpy as np
-from scipy.stats import chisquare
-import matplotlib.pyplot as plt
-import pickle
-import matplotlib
-
-# read data
-
-pkl_file = open('normalized_flux.pkl', 'rb')
-nor = pickle.load(pkl_file)
-pkl_file.close()
-
-pkl_file = open('inferred_spectrum.pkl', 'rb')
-inf_old = pickle.load(pkl_file)
-pkl_file.close()
-
-pkl_file = open('optimized_spectrum.pkl', 'rb')
-inf_opt = pickle.load(pkl_file)
-pkl_file.close()
-
-
-"""
-nor = nor.ravel()
-inf_old = inf_old.ravel()
-inf_opt = inf_opt.ravel()
-
-"""
-chi_old = np.array([])
-p_old = np.array([])
-
-chi_opt = np.array([])
-p_opt = np.array([])
-
-for p in range(0,nor[:,0].size):
-    nor = nor[p, :]
-    inf_old = inf_old[p, :]
-    inf_opt = inf_opt[p, :]
-    a_old, b_old = chisquare(nor, inf_old)
-    a_opt, b_opt = chisquare((nor, inf_opt))
-    np.append(chi_old,a_old)
-    #p_old.append(b_old)
-    np.append(chi_opt,a_opt)
-    #p_old.append(b_opt)
-
 # plot
 wl = np.ones(nor[:,0].size)
 
@@ -61,13 +17,3 @@ plt.ylabel('chi-square', fontsize=36)
 #axes.set_ylim([0.8,1.1])
 #axes.set_ylim([0.8,1.1])
 plt.show()
-
-
-
-
-
-
-
-
-
-
